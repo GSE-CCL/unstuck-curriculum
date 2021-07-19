7bb6c4b04c9837add5df851f91ee49d43d97f746
jhawson <jhawson@gmail.com>
2021-07-19 19:33:32 -0400

    Fix comment typo

--- node-postinstall.sh
+++ node-postinstall.sh
@@ -2,6 +2,6 @@
 
 # This script is called by package.json after intstalling any node modules. 
 
-# Copy BootStrap Javascript output from node node plugin folder to appropriate site content folder.  
+# Copy BootStrap Javascript output from node plugin folder to appropriate site content folder.  
 cp node_modules/bootstrap/dist/js/bootstrap.min.* app/static/js/
 
