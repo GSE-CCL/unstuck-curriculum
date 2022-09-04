514ab0b206649b5bbb1d28c32f5d2797ae475b58
KB <karen.brennan@gmail.com>
2022-09-04 14:39:58 -0400

    Added the links for the Español and Français translations.

--- app/content/translationinfo.py
+++ app/content/translationinfo.py
@@ -12,15 +12,60 @@ unstuck_translations = {
         "localized_edit" : "EDITAR", 
         "localized_download" : "DESCARGAR", 
         "modules" : [{
-            "title" : "First title", 
-            "preview" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/view", 
-            "edit" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/copy", 
-            "download" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/export/pdf", 
+            "title" : "Orientación", 
+            "preview" : "https://docs.google.com/presentation/d/1DnORf7P9son_Hk5QeF7Q8BvgfLKuLfqGSbyyFe8ef3E/view", 
+            "edit" : "https://docs.google.com/presentation/d/1DnORf7P9son_Hk5QeF7Q8BvgfLKuLfqGSbyyFe8ef3E/copy", 
+            "download" : "https://docs.google.com/presentation/d/1DnORf7P9son_Hk5QeF7Q8BvgfLKuLfqGSbyyFe8ef3E/export/pdf", 
             }, {
-            "title" : "Second title", 
-            "preview" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/view", 
-            "edit" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/copy", 
-            "download" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/export/pdf", 
+            "title" : "Al Hacer Clic", 
+            "preview" : "https://docs.google.com/presentation/d/1ttELL6p9k857brOFPWUNjKAvUwjLDMyG5KEqGp2ma5M/view", 
+            "edit" : "https://docs.google.com/presentation/d/1ttELL6p9k857brOFPWUNjKAvUwjLDMyG5KEqGp2ma5M/copy", 
+            "download" : "https://docs.google.com/presentation/d/1ttELL6p9k857brOFPWUNjKAvUwjLDMyG5KEqGp2ma5M/export/pdf", 
+            },  {
+            "title" : "Paralelismo", 
+            "preview" : "https://docs.google.com/presentation/d/1lQnvA2lfNrAA0bBeCBWfhVoHpb4nQinduLLjTeHXE1U/view", 
+            "edit" : "https://docs.google.com/presentation/d/1lQnvA2lfNrAA0bBeCBWfhVoHpb4nQinduLLjTeHXE1U/copy", 
+            "download" : "https://docs.google.com/presentation/d/1lQnvA2lfNrAA0bBeCBWfhVoHpb4nQinduLLjTeHXE1U/export/pdf", 
+            },  {
+            "title" : "Presionar tecla", 
+            "preview" : "https://docs.google.com/presentation/d/1vn0iVjrSmHKA3xVblY3NkTHHS5M7swzF-v0C2uCirYs/view", 
+            "edit" : "https://docs.google.com/presentation/d/1vn0iVjrSmHKA3xVblY3NkTHHS5M7swzF-v0C2uCirYs/copy", 
+            "download" : "https://docs.google.com/presentation/d/1vn0iVjrSmHKA3xVblY3NkTHHS5M7swzF-v0C2uCirYs/export/pdf", 
+            },  {
+            "title" : "Bucles", 
+            "preview" : "https://docs.google.com/presentation/d/1dRFVU-PH1ftePDhZxOeZIZ66r8wEYJnPbS5fsvV0eEQ/view", 
+            "edit" : "https://docs.google.com/presentation/d/1dRFVU-PH1ftePDhZxOeZIZ66r8wEYJnPbS5fsvV0eEQ/copy", 
+            "download" : "https://docs.google.com/presentation/d/1dRFVU-PH1ftePDhZxOeZIZ66r8wEYJnPbS5fsvV0eEQ/export/pdf", 
+            },  {
+            "title" : "Mensajes", 
+            "preview" : "https://docs.google.com/presentation/d/1XqakOx6t5XeYlGHxBn5vsoeozxM_5Uo9Wdrm_XgvsqE/view", 
+            "edit" : "https://docs.google.com/presentation/d/1XqakOx6t5XeYlGHxBn5vsoeozxM_5Uo9Wdrm_XgvsqE/copy", 
+            "download" : "https://docs.google.com/presentation/d/1XqakOx6t5XeYlGHxBn5vsoeozxM_5Uo9Wdrm_XgvsqE/export/pdf", 
+            },  {
+            "title" : "Tocando Color", 
+            "preview" : "https://docs.google.com/presentation/d/19ME_JE1XUiHC8XSizL1oUV4-DBDIAAtp6LVY-SsxEwA/view", 
+            "edit" : "https://docs.google.com/presentation/d/19ME_JE1XUiHC8XSizL1oUV4-DBDIAAtp6LVY-SsxEwA/copy", 
+            "download" : "https://docs.google.com/presentation/d/19ME_JE1XUiHC8XSizL1oUV4-DBDIAAtp6LVY-SsxEwA/export/pdf", 
+            },  {
+            "title" : "Aleatorio", 
+            "preview" : "https://docs.google.com/presentation/d/1EGyFY43vO0Ke69bAdG3AaAe5stBRIYptiGykfgZKNrs/view", 
+            "edit" : "https://docs.google.com/presentation/d/1EGyFY43vO0Ke69bAdG3AaAe5stBRIYptiGykfgZKNrs/copy", 
+            "download" : "https://docs.google.com/presentation/d/1EGyFY43vO0Ke69bAdG3AaAe5stBRIYptiGykfgZKNrs/export/pdf", 
+            },  {
+            "title" : "Pregunta Y Respuesta", 
+            "preview" : "https://docs.google.com/presentation/d/1gWMII_WBmY2BPSIFKJlOW7e3W1oMDgUN5yNKLVnIHoY/view", 
+            "edit" : "https://docs.google.com/presentation/d/1gWMII_WBmY2BPSIFKJlOW7e3W1oMDgUN5yNKLVnIHoY/copy", 
+            "download" : "https://docs.google.com/presentation/d/1gWMII_WBmY2BPSIFKJlOW7e3W1oMDgUN5yNKLVnIHoY/export/pdf", 
+            },  {
+            "title" : "Variables", 
+            "preview" : "https://docs.google.com/presentation/d/1C3ExCiCB2LU6BF_QIxXirQgdSwIMssL2uCMhweSGnOo/view", 
+            "edit" : "https://docs.google.com/presentation/d/1C3ExCiCB2LU6BF_QIxXirQgdSwIMssL2uCMhweSGnOo/copy", 
+            "download" : "https://docs.google.com/presentation/d/1C3ExCiCB2LU6BF_QIxXirQgdSwIMssL2uCMhweSGnOo/export/pdf", 
+            },  {
+            "title" : "Listas", 
+            "preview" : "https://docs.google.com/presentation/d/1e2k9WK4wkSRI75R1GYQzn9yx0VsstFIfj9A6lCDfy0U/view", 
+            "edit" : "https://docs.google.com/presentation/d/1e2k9WK4wkSRI75R1GYQzn9yx0VsstFIfj9A6lCDfy0U/copy", 
+            "download" : "https://docs.google.com/presentation/d/1e2k9WK4wkSRI75R1GYQzn9yx0VsstFIfj9A6lCDfy0U/export/pdf", 
             },             
         ]
     },
@@ -30,16 +75,56 @@ unstuck_translations = {
         "localized_edit" : "ÉDITER", 
         "localized_download" : "TÉLÉCHARGER",         
         "modules" : [{
-            "title" : "First title", 
-            "preview" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/view", 
-            "edit" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/copy", 
-            "download" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/export/pdf", 
+            "title" : "Quand...Est Cliqué", 
+            "preview" : "https://docs.google.com/presentation/d/1roh3Xa7Dbyg6x7hOWPHSJGKL5y5IXTnZgIv0S8seGJY/view", 
+            "edit" : "https://docs.google.com/presentation/d/1roh3Xa7Dbyg6x7hOWPHSJGKL5y5IXTnZgIv0S8seGJY/copy", 
+            "download" : "https://docs.google.com/presentation/d/1roh3Xa7Dbyg6x7hOWPHSJGKL5y5IXTnZgIv0S8seGJY/export/pdf", 
             }, {
-            "title" : "Second title", 
-            "preview" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/view", 
-            "edit" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/copy", 
-            "download" : "https://docs.google.com/presentation/d/1ftWO-EqdDS3Mz7xFvoy8AD50qSjdfr8vgUgW0q9hQI8/export/pdf", 
-            },             
+            "title" : "Instructions Parallèles", 
+            "preview" : "https://docs.google.com/presentation/d/1MXtZjyH7Nd7Dk6wH50a4R2liukf0e_rsZWW2mOZIfts/view", 
+            "edit" : "https://docs.google.com/presentation/d/1MXtZjyH7Nd7Dk6wH50a4R2liukf0e_rsZWW2mOZIfts/copy", 
+            "download" : "https://docs.google.com/presentation/d/1MXtZjyH7Nd7Dk6wH50a4R2liukf0e_rsZWW2mOZIfts/export/pdf", 
+            },  {
+            "title" : "Quand On Appuie Sur La Touche", 
+            "preview" : "https://docs.google.com/presentation/d/1xCeV64DMOKtUlPi3p8CEEERSTQlRm1cT4-fQIPcFigc/view", 
+            "edit" : "https://docs.google.com/presentation/d/1xCeV64DMOKtUlPi3p8CEEERSTQlRm1cT4-fQIPcFigc/copy", 
+            "download" : "https://docs.google.com/presentation/d/1xCeV64DMOKtUlPi3p8CEEERSTQlRm1cT4-fQIPcFigc/export/pdf", 
+            },  {
+            "title" : "Boucles", 
+            "preview" : "https://docs.google.com/presentation/d/1LsuvdEK3v6WURXaywSsTjFqaWVWW8LJF2rfrrlzU2ks/view", 
+            "edit" : "https://docs.google.com/presentation/d/1LsuvdEK3v6WURXaywSsTjFqaWVWW8LJF2rfrrlzU2ks/copy", 
+            "download" : "https://docs.google.com/presentation/d/1LsuvdEK3v6WURXaywSsTjFqaWVWW8LJF2rfrrlzU2ks/export/pdf", 
+            },  {
+            "title" : "Envoyer Un Message", 
+            "preview" : "https://docs.google.com/presentation/d/18XvOCdyF0HxiSxzV2ev7jMSb3X9Uzzp7QGHj5VVJqqY/view", 
+            "edit" : "https://docs.google.com/presentation/d/18XvOCdyF0HxiSxzV2ev7jMSb3X9Uzzp7QGHj5VVJqqY/copy", 
+            "download" : "https://docs.google.com/presentation/d/18XvOCdyF0HxiSxzV2ev7jMSb3X9Uzzp7QGHj5VVJqqY/export/pdf", 
+            },  {
+            "title" : "Couleur Touchée", 
+            "preview" : "https://docs.google.com/presentation/d/1o0DLlWSABrCpSjLkyrhoZkVVZ5NT8VcZwVXHDS0FHQs/view", 
+            "edit" : "https://docs.google.com/presentation/d/1o0DLlWSABrCpSjLkyrhoZkVVZ5NT8VcZwVXHDS0FHQs/copy", 
+            "download" : "https://docs.google.com/presentation/d/1o0DLlWSABrCpSjLkyrhoZkVVZ5NT8VcZwVXHDS0FHQs/export/pdf", 
+            },  {
+            "title" : "Nombres Aléatoires", 
+            "preview" : "https://docs.google.com/presentation/d/1H-OL3LjJu_uoA0lIC15Wg8pClRDUPXKsOmDUVSpbGDs/view", 
+            "edit" : "https://docs.google.com/presentation/d/1H-OL3LjJu_uoA0lIC15Wg8pClRDUPXKsOmDUVSpbGDs/copy", 
+            "download" : "https://docs.google.com/presentation/d/1H-OL3LjJu_uoA0lIC15Wg8pClRDUPXKsOmDUVSpbGDs/export/pdf", 
+            },  {
+            "title" : "Demander Et Attendre", 
+            "preview" : "https://docs.google.com/presentation/d/1bf8S45AIE-jJ_JgrTu6m9g1q-r4W9q2q5xUES-AIJw8/view", 
+            "edit" : "https://docs.google.com/presentation/d/1bf8S45AIE-jJ_JgrTu6m9g1q-r4W9q2q5xUES-AIJw8/copy", 
+            "download" : "https://docs.google.com/presentation/d/1bf8S45AIE-jJ_JgrTu6m9g1q-r4W9q2q5xUES-AIJw8/export/pdf", 
+            },  {
+            "title" : "Variables", 
+            "preview" : "https://docs.google.com/presentation/d/1syoYpSohn4rP3vhLKbp3UaqBn-HYMiY1FxZM_V8CcKw/view", 
+            "edit" : "https://docs.google.com/presentation/d/1syoYpSohn4rP3vhLKbp3UaqBn-HYMiY1FxZM_V8CcKw/copy", 
+            "download" : "https://docs.google.com/presentation/d/1syoYpSohn4rP3vhLKbp3UaqBn-HYMiY1FxZM_V8CcKw/export/pdf", 
+            },  {
+            "title" : "Listes", 
+            "preview" : "https://docs.google.com/presentation/d/1ISGQQtl4QzwOKiL5lp9JoT_OT0K0OPQ7K-3XkCpwHwI/view", 
+            "edit" : "https://docs.google.com/presentation/d/1ISGQQtl4QzwOKiL5lp9JoT_OT0K0OPQ7K-3XkCpwHwI/copy", 
+            "download" : "https://docs.google.com/presentation/d/1ISGQQtl4QzwOKiL5lp9JoT_OT0K0OPQ7K-3XkCpwHwI/export/pdf", 
+            },      
         ]
     }
 }
